f3661254164f568fb0c5da1e5880bc3c82cd37c2
mabbuprem <mabbupremkumar@gmail.com>
2022-04-27 17:28:07 +0530

    [prem] FEATURE ADDED: Welcome to product reviev management

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+
+namespace Product_Review_Management_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product_Review_Management_LINQ");
+        }
+    }
+}
+            
+            
+
