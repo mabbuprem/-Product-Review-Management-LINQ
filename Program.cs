6c9d379bfd29b756ce88f95d5247e8c98db9d5fb
mabbuprem <mabbupremkumar@gmail.com>
2022-05-01 16:07:33 +0530

    [prem] FEATURE ADDED:Retriving Top 3 Records by Product Rating

--- Program.cs
+++ Program.cs
@@ -11,14 +11,29 @@ namespace Product_Review_Management_LINQ
         {
             ProductReviewManager manager = new ProductReviewManager();
             List<ProductReview> productReviewFilledList = new List<ProductReview>();
+            Console.WriteLine("Enter 1 to Write all Product Review Values in Console\nEnter 2 to retrieve top 3 Records as per rating");
+            int UC = Convert.ToInt32(Console.ReadLine());
             productReviewFilledList = manager.AddingValuesInProductReviewList(productReviewFilledList);
-            foreach (ProductReview productReview in productReviewFilledList)
+            switch (UC)
+            {
+                case 1:
+                    PrintListToConsole(productReviewFilledList);
+                    break;
+                case 2:
+                    var result = productReviewFilledList.OrderByDescending(x => x.rating).Take(3).ToList();
+                    PrintListToConsole(result);
+                    break;
+            }
+        }
+        public static void PrintListToConsole(List<ProductReview> printedList)
+        {
+            foreach (ProductReview productReview in printedList)
             {
                 Console.WriteLine(productReview.ToString());
             }
         }
     }
 }
-            
-            
+
+
 
