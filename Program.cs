ae9daec4076cdeb5b89260b52f2d7cec0a1be4d9
mabbuprem <mabbupremkumar@gmail.com>
2022-05-03 13:14:16 +0530

    [prem} FEATURE ADDED:Retrieve Records whose review is Very Good

--- Program.cs
+++ Program.cs
@@ -11,9 +11,13 @@ namespace Product_Review_Management_LINQ
         {
             ProductReviewManager manager = new ProductReviewManager();
             List<ProductReview> productReviewFilledList = new List<ProductReview>();
-            Console.WriteLine("Enter 1 to Write all Product Review Values in Console\nEnter 2 to retrieve top 3 Records as per rating\nEnter 3 to retrieve Records with product ID & rating greater than 3\nEnter 4 to retrive record counts when grouped by ProductID\nEnter 5 to retrive product ID & Product Review\nEnter 6 to skip 5 records & retrieve remaining\nEnter 7 to retrieve Product ID & review by Select LINQ\nEnter 8 to add product reviews in datatable\nEnter 9 to retrive Average Rating grouped by product ID from Datatable");
+            Console.WriteLine("Enter 1 to Write all Product Review Values in Console\nEnter 2 to retrieve top 3 Records as per rating\nEnter 3 to retrieve Records with product ID & rating greater than 3" +
+                "\nEnter 4 to retrive record counts when grouped by ProductID\nEnter 5 to retrive product ID & Product Review\nEnter 6 to skip 5 records & retrieve remaining" +
+                "\nEnter 7 to retrieve Product ID & review by Select LINQ\nEnter 8 to add product reviews in datatable\nEnter 9 to retrieve records whose isLike = true" +
+                "\nEnter 10 to retrive Average Rating grouped by product ID from Datatable\nEnter 11 to retrieve records whose review message is Very Good");
             int UC = Convert.ToInt32(Console.ReadLine());
             productReviewFilledList = manager.AddingValuesInProductReviewList(productReviewFilledList);
+            var filledDataTable = ProductReviewManager.AddingDefaultValueswithDataTable(productReviewFilledList);
             switch (UC)
             {
                 case 1:
@@ -48,29 +52,32 @@ namespace Product_Review_Management_LINQ
                     PrintListToConsole(result6);
                     break;
                 case 8:
-                    var result8 = ProductReviewManager.AddingDefaultValueswithDataTable(productReviewFilledList);
-                    foreach (DataRow row in result8.Rows)
-                    {
-                        for (int i = 0; i < row.ItemArray.Length; i++)
-                        {
-                            Console.Write(row.ItemArray[i] + ",");
-                        }
-                        Console.WriteLine();
-                    }
+                    PrintDataTableEnumerableToConsole(filledDataTable.AsEnumerable());
                     break;
                 case 9:
-                    var filledDataTable = ProductReviewManager.AddingDefaultValueswithDataTable(productReviewFilledList);
                     var result9 = from ProductReview in filledDataTable.AsEnumerable()
-                                  group ProductReview by ProductReview.Field<int>("productID") into productReviewGroupByID
-                                  select new
-                                  {
-                                      productID = productReviewGroupByID.Key,
-                                      AverageRating = productReviewGroupByID.Average(x => x.Field<double>("rating")),
-                                  };
-                    foreach (var item in result9)
+                                  where ProductReview.Field<bool>("isLike") == true
+                                  select ProductReview;
+                    PrintDataTableEnumerableToConsole(result9);
+                    break;
+                case 10:
+                    var result10 = from ProductReview in filledDataTable.AsEnumerable()
+                                   group ProductReview by ProductReview.Field<int>("productID") into productReviewGroupByID
+                                   select new
+                                   {
+                                       productID = productReviewGroupByID.Key,
+                                       AverageRating = productReviewGroupByID.Average(x => x.Field<double>("rating")),
+                                   };
+                    foreach (var item in result10)
                     {
                         Console.WriteLine($"ProductID = {item.productID}, AverageRating = {item.AverageRating}");
                     }
+                    break;
+                case 11:
+                    var result11 = from ProductReview in filledDataTable.AsEnumerable()
+                                   where ProductReview.Field<string>("review") == "Very Good"
+                                   select ProductReview;
+                    PrintDataTableEnumerableToConsole(result11);
                     break;
             }
         }
@@ -79,6 +86,14 @@ namespace Product_Review_Management_LINQ
             foreach (ProductReview productReview in printedList)
             {
                 Console.WriteLine(productReview.ToString());
+            }
+        }
+        public static void PrintDataTableEnumerableToConsole(EnumerableRowCollection<DataRow> result)
+        {
+            Console.WriteLine($"ProductId,  UserId,  Rating,  Review,  IsLike");
+            foreach (var row in result)
+            {
+                Console.WriteLine($"{row["ProductId"]},  {row["UserId"]},  {row["Rating"]},  {row["Review"]},  {row["IsLike"]}");
             }
         }
     }
