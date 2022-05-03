7a768a1bbe9c04d185d1c7fe71945bba8c74aa5a
mabbuprem <mabbupremkumar@gmail.com>
2022-05-03 13:00:09 +0530

    [prem] FEATURE ADDED:Retriving Product ID & Review by Select LINQ

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Product_Review_Management_LINQ
         {
             ProductReviewManager manager = new ProductReviewManager();
             List<ProductReview> productReviewFilledList = new List<ProductReview>();
-            Console.WriteLine("Enter 1 to Write all Product Review Values in Console\nEnter 2 to retrieve top 3 Records as per rating\nEnter 3 to retrieve Records with product ID & rating greater than 3\nEnter 4 to retrive record counts when grouped by ProductID\nEnter 5 to retrive product ID & Product Review\nEnter 6 to skip 5 records & retrieve remaining.");
+            Console.WriteLine("Enter 1 to Write all Product Review Values in Console\nEnter 2 to retrieve top 3 Records as per rating\nEnter 3 to retrieve Records with product ID & rating greater than 3\nEnter 4 to retrive record counts when grouped by ProductID\nEnter 5 to retrive product ID & Product Review\nEnter 6 to skip 5 records & retrieve remaining\nEnter 7 to retrieve Product ID & review by Select LINQ");
             int UC = Convert.ToInt32(Console.ReadLine());
             productReviewFilledList = manager.AddingValuesInProductReviewList(productReviewFilledList);
             switch (UC)
@@ -36,6 +36,7 @@ namespace Product_Review_Management_LINQ
                     }
                     break;
                 case 5:
+                case 7:
                     var result5 = from ProductReview in productReviewFilledList select new { ProductReview.productID, ProductReview.review };
                     foreach (var item in result5)
                     {
